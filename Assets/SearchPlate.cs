6f3b3ad8a8a6376e4fc5d031127c7b87b1b72e8d
V1NDs <65359820+V1NDs@users.noreply.github.com>
2025-01-13 15:25:19 +0100

    StartProjekt

---
+++ Assets/SearchPlate.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SearchPlate : MonoBehaviour
+{
+    public GameObject plateInput;
+
+    public void Search()
+    {
+        var carPlate = plateInput.GetComponent<TMP_InputField>().text;
+
+        StartCoroutine(GetRequest("https://www.nummerplade.net/nummerplade/" + carPlate + ".html"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+}
